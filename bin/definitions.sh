0d05fe1120169c3f1cd42266268d47c928698269
Owen Dando <owen.dando@ed.ac.uk>
2014-11-14 14:03:02 +0000

    Quantify with Sailfish.

--- bin/definitions.sh
+++ bin/definitions.sh
@@ -6,6 +6,7 @@ MAPPED_READS_DIR=mapped_reads
 QUANT_RESULTS_DIR=quant_results
 
 BOWTIE_INDEX=hs_bowtie_index/per_contig
+SAILFISH_INDEX_DIR=genome_reference/sailfish
 
 SINGLE_END_SAMPLES="bullard_uhr bullard_hbr"
 PAIRED_END_SAMPLES="rapaport_uhr_1 rapaport_uhr_2 rapaport_uhr_3 rapaport_uhr_4 rapaport_uhr_5 rapaport_hbr_1 rapaport_hbr_2 rapaport_hbr_3 rapaport_hbr_4 rapaport_hbr_5 au_hbr"
--- bin/run_quantification_tools.sh
+++ bin/run_quantification_tools.sh
@@ -4,15 +4,49 @@ source bin/definitions.sh
 
 function quantify_with_cufflinks {
     SAMPLE=$1
+    MAPPED_READS=$2
     
     OUTPUT_DIR=cufflinks.$(get_random_id)
-    cufflinks -o $OUTPUT_DIR -u -b ${BOWTIE_INDEX}.fa -p 8 --library-type fr-unstranded -G ${DATA_DIR}/human_protein_coding.gtf ${MAPPED_READS_DIR}/${SAMPLE}.bam
+
+    cufflinks \
+        -p 8 \
+        -u -b ${BOWTIE_INDEX}.fa  \
+        --library-type fr-unstranded \
+        -G ${DATA_DIR}/human_protein_coding.gtf \
+        -o $OUTPUT_DIR \
+        $MAPPED_READS
+
     mv $OUTPUT_DIR/genes.fpkm_tracking ${QUANT_RESULTS_DIR}/${SAMPLE}.cufflinks_fpkm
+    rm -rf $OUTPUT_DIR
+}
+
+function quantify_with_sailfish {
+    SAMPLE=$1
+
+    OUTPUT_DIR=sailfish.$(get_random_id)
+
+    if [ "$#" -eq 2 ]; then
+        # Single-end reads
+        READS_SPEC="-l T=SE:S=U -r $2"
+    elif [ "$#" -eq 3 ]; then
+        # Paired-end reads
+        READS_SPEC="-l T=PE:O=><:S=U -1 $2 -2 $3"
+    fi
+
+    sailfish quant -p 8 -i $SAILFISH_INDEX_DIR $READS_SPEC -o $OUTPUT_DIR
+
+    mv $OUTPUT_DIR/quant_bias_corrected.sf ${QUANT_RESULTS_DIR}/${SAMPLE}.sailfish_tpm
     rm -rf $OUTPUT_DIR
 }
 
 mkdir -p $QUANT_RESULTS_DIR
 
-for sample in $SINGLE_END_SAMPLES $PAIRED_END_SAMPLES; do
-    quantify_with_cufflinks $sample
+for sample in $SINGLE_END_SAMPLES; do
+    quantify_with_cufflinks $sample ${MAPPED_READS_DIR}/${sample}.bam
+    quantify_with_sailfish $sample ${RNA_SEQ_DIR}/${sample}.fastq
+done
+
+for sample in $PAIRED_END_SAMPLES; do
+    quantify_with_cufflinks $sample ${MAPPED_READS_DIR}/${sample}.bam
+    quantify_with_sailfish $sample ${RNA_SEQ_DIR}/${sample}.1.fastq ${RNA_SEQ_DIR}/${sample}.2.fastq
 done
